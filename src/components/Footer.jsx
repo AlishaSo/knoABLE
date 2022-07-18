e97392b2da31dbd94933ee81a0bfe28b93d44c1f
Alisha Soriano <sorianoalisha@gmail.com>
2022-07-18 19:26:47 -0400

    Add the code and assets for the header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
+import {NavLink} from 'react-router-dom';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHouse} from '@fortawesome/free-solid-svg-icons';
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
+import {faShuffle} from '@fortawesome/free-solid-svg-icons';
+
 export default function Footer() {
   return (
-    <div>
-
-    </div>
+    <footer>
+      <NavLink to='/'><FontAwesomeIcon className='fa-icon' icon={faHouse} size='2x' inverse /></NavLink>
+      <NavLink to='/find'><FontAwesomeIcon className='fa-icon' icon={faMagnifyingGlass} size='2x' inverse /></NavLink>
+      <NavLink to='/discover'><FontAwesomeIcon className='fa-icon' icon={faShuffle} size='2x' inverse /></NavLink>
+    </footer>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
-
+import {NavLink} from 'react-router-dom';
+import logo from '../images/logo.png';
 
 export default function Header() {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <img className='logo' src={logo} alt="knoable app logo, stylized with 'kno' in lowercase, and 'able' in uppercase. Next to the text, there's an image of a brain. The whole logo is a light blue color" />
+      <nav>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/find'>Find</NavLink>
+        <NavLink to='/discover'>Discover</NavLink>
+      </nav>
+    </header>
   )
 }
