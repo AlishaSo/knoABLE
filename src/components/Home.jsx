b791ed587b23eda9768aed46bb2db0ed7067e9c1
Alisha Soriano <sorianoalisha@gmail.com>
2022-07-19 13:14:04 -0400

    Add a classname to the home component div and a header inside the body

---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='home-div'>
+      <h3>Want to know if that book you read/want to read is a NYT bestseller? Use this app to find out!</h3>
+    </div>
+  )
+}
