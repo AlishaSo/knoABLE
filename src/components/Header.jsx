5946c6df6a02fd102497f8ece9cc4d3c6f0092e0
Alisha Soriano <sorianoalisha@gmail.com>
2022-07-19 10:40:13 -0400

    Wrap the logo in a navlink to lead to the home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import logo from '../images/logo.png';
 export default function Header() {
   return (
     <header>
-      <img className='logo' src={logo} alt="knoable app logo, stylized with 'kno' in lowercase, and 'able' in uppercase. Next to the text, there's an image of a brain. The whole logo is a light blue color" />
+      <NavLink to='/'><img className='logo' src={logo} alt="knoable app logo, stylized with 'kno' in lowercase, and 'able' in uppercase. Next to the text, there's an image of a brain. The whole logo is a light blue color" /></NavLink>
       <nav>
         <NavLink to='/'>Home</NavLink>
         <NavLink to='/find'>Find</NavLink>
