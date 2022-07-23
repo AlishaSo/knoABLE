1f47342f1378bf408c604a1edcde972fc9bc146c
Alisha Soriano <sorianoalisha@gmail.com>
2022-07-22 23:28:28 -0400

    Add the date input and button to get the date from the user for the fetch request, get the data and conditionally render the RandomBooks component

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,7 +1,37 @@
-export default function Discover() {
-  return (
-    <div>
+import {getRandomBook} from '../services/api';
+import {useState} from 'react';
+import RandomBooks from './RandomBook';
 
+export default function Discover() {
+  const [bookInfo, setBookInfo] = useState({});
+  const todaysDate = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const userInputDate = event.target.dateSelect.value;
+
+    getData(userInputDate);
+  }
+  
+  async function getData(date) {
+    const data = await getRandomBook(date);
+    console.log(data)
+    setBookInfo(data);
+  }
+
+  return (
+    <div className='discover-div'>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='date'
+          name='dateSelect'
+          min='2008-06-22'
+          max={todaysDate}
+        />
+        <button>Get a NYT bestseller</button>
+      </form>
+      
+      {Object.keys(bookInfo).length > 0 && <RandomBooks bookInfo={bookInfo} />}
     </div>
   )
 }
