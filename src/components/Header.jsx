8cc0f50bef6bcf198e330dba39736eb9b73b52cc
Alisha Soriano <sorianoalisha@gmail.com>
2022-07-23 15:58:48 -0400

    Add elements needed for hamburguer menu and create function for functionality to toggle the menu upon button click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,24 @@ import {NavLink} from 'react-router-dom';
 import logo from '../images/logo.png';
 
 export default function Header() {
+  function toggleMenu() {
+    const navBar = document.querySelector('nav');
+    navBar.classList.toggle('visible');
+  }
+
   return (
     <header>
-      <NavLink to='/'><img className='logo' src={logo} alt="knoable app logo, stylized with 'kno' in lowercase, and 'able' in uppercase. Next to the text, there's an image of a brain. The whole logo is a light blue color" /></NavLink>
-      <nav>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/find'>Find</NavLink>
-        <NavLink to='/discover'>Discover</NavLink>
-      </nav>
+      <div>
+        <button className='nav-toggle' onClick={toggleMenu}>
+          <span className='hamburguer'></span>
+        </button>
+        <NavLink to='/'><img className='logo' src={logo} alt="knoable app logo, stylized with 'kno' in lowercase, and 'able' in uppercase. Next to the text, there's an image of a brain. The whole logo is a light blue color" /></NavLink>
+        <nav>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/find'>Find</NavLink>
+          <NavLink to='/discover'>Discover</NavLink>
+        </nav>
+      </div>
     </header>
   )
 }
