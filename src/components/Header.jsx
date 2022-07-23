a887b0a826a3b65215b3ff7d8c73a691093ba0f1
Alisha Soriano <sorianoalisha@gmail.com>
2022-07-23 17:18:10 -0400

    Switch to using a state variable to keep track of the visible class to show/hide the menu on mobile and add functionality for close menu if clicking outside of it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,29 @@
 import {NavLink} from 'react-router-dom';
 import logo from '../images/logo.png';
+import {useState, useEffect} from 'react';
 
 export default function Header() {
-  function toggleMenu() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
     const navBar = document.querySelector('nav');
-    navBar.classList.toggle('visible');
-  }
+    const closeMenu = e => {
+      if(e.originalTarget.tagName != 'BUTTON' && e.originalTarget.tagName != 'SPAN')
+        setMenuOpen(false);
+    }
+    document.body.addEventListener('click', closeMenu);
+
+    return () => document.body.removeEventListener('click', closeMenu);
+  }, []);
 
   return (
     <header>
       <div>
-        <button className='nav-toggle' onClick={toggleMenu}>
+        <button className='nav-toggle' onClick={() => setMenuOpen(prevMenuOpen => !prevMenuOpen)}>
           <span className='hamburguer'></span>
         </button>
         <NavLink to='/'><img className='logo' src={logo} alt="knoable app logo, stylized with 'kno' in lowercase, and 'able' in uppercase. Next to the text, there's an image of a brain. The whole logo is a light blue color" /></NavLink>
-        <nav>
+        <nav className={`${menuOpen ? 'visible' : ''}`}>
           <NavLink to='/'>Home</NavLink>
           <NavLink to='/find'>Find</NavLink>
           <NavLink to='/discover'>Discover</NavLink>
