28ac6be5b0cc3343dd2c3b03f8db51abf4ccff1e
Alisha Soriano <sorianoalisha@gmail.com>
2022-07-23 17:20:11 -0400

    Add a separate class to each NavLink

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ import {faShuffle} from '@fortawesome/free-solid-svg-icons';
 export default function Footer() {
   return (
     <footer>
-      <NavLink to='/'><FontAwesomeIcon className='fa-icon' icon={faHouse} size='2x' inverse /></NavLink>
-      <NavLink to='/find'><FontAwesomeIcon className='fa-icon' icon={faMagnifyingGlass} size='2x' inverse /></NavLink>
-      <NavLink to='/discover'><FontAwesomeIcon className='fa-icon' icon={faShuffle} size='2x' inverse /></NavLink>
+      <NavLink to='/' className='footer-link'><FontAwesomeIcon className='fa-icon' icon={faHouse} size='2x' inverse /></NavLink>
+      <NavLink to='/find' className='footer-link'><FontAwesomeIcon className='fa-icon' icon={faMagnifyingGlass} size='2x' inverse /></NavLink>
+      <NavLink to='/discover' className='footer-link'><FontAwesomeIcon className='fa-icon' icon={faShuffle} size='2x' inverse /></NavLink>
     </footer>
   )
 }
