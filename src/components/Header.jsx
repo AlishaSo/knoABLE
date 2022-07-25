8fc685d8f42304051a150c51e208fa360b24bb65
Alisha Soriano <sorianoalisha@gmail.com>
2022-07-25 00:44:56 -0400

    Fix errors that are causing it to not be build the deploy on Netlify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,8 @@ export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
-    const navBar = document.querySelector('nav');
     const closeMenu = e => {
-      if(e.originalTarget.tagName != 'BUTTON' && e.originalTarget.tagName != 'SPAN')
+      if(e.originalTarget.tagName !== 'BUTTON' && e.originalTarget.tagName !== 'SPAN')
         setMenuOpen(false);
     }
     document.body.addEventListener('click', closeMenu);
@@ -22,7 +21,7 @@ export default function Header() {
         <button className='nav-toggle' onClick={() => setMenuOpen(prevMenuOpen => !prevMenuOpen)}>
           <span className='hamburguer'></span>
         </button>
-        <NavLink to='/'><img className='logo' src={logo} alt="knoable app logo, stylized with 'kno' in lowercase, and 'able' in uppercase. Next to the text, there's an image of a brain. The whole logo is a light blue color" /></NavLink>
+        <NavLink to='/'><img className='logo' src={logo} alt="knoable app logo, stylized with 'kno' in lowercase, and 'able' in uppercase. Next to the text, there's an illustration of a brain. The whole logo is a light blue color" /></NavLink>
         <nav className={`${menuOpen ? 'visible' : ''}`}>
           <NavLink to='/'>Home</NavLink>
           <NavLink to='/find'>Find</NavLink>
