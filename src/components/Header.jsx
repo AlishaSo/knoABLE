a5b128192225638c31a0ae0d3957e9af4542826a
Alisha Soriano <sorianoalisha@gmail.com>
2022-07-24 22:16:15 -0400

    Wrap the containers of <header> inside a new div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,15 +18,17 @@ export default function Header() {
 
   return (
     <header>
-      <button className='nav-toggle' onClick={() => setMenuOpen(prevMenuOpen => !prevMenuOpen)}>
-        <span className='hamburguer'></span>
-      </button>
-      <NavLink to='/'><img className='logo' src={logo} alt="knoable app logo, stylized with 'kno' in lowercase, and 'able' in uppercase. Next to the text, there's an image of a brain. The whole logo is a light blue color" /></NavLink>
-      <nav className={`${menuOpen ? 'visible' : ''}`}>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/find'>Find</NavLink>
-        <NavLink to='/discover'>Discover</NavLink>
-      </nav>
+      <div className='wrapper'>
+        <button className='nav-toggle' onClick={() => setMenuOpen(prevMenuOpen => !prevMenuOpen)}>
+          <span className='hamburguer'></span>
+        </button>
+        <NavLink to='/'><img className='logo' src={logo} alt="knoable app logo, stylized with 'kno' in lowercase, and 'able' in uppercase. Next to the text, there's an image of a brain. The whole logo is a light blue color" /></NavLink>
+        <nav className={`${menuOpen ? 'visible' : ''}`}>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/find'>Find</NavLink>
+          <NavLink to='/discover'>Discover</NavLink>
+        </nav>
+      </div>
     </header>
   )
 }
